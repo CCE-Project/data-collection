fc473891d201e8d442c6abe0c08f94bc60548711
xelathan <alex.t.tran@gmail.com>
2024-01-21 20:52:40 -0800

    Synchornous

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ import csv
 with open('./config.json', 'r') as f:
     config = json.load(f)
 
+PAGE_RETRIES = 5
+
 uri = config['mongodb_connection_string']
 mongo_client = MongoClient(uri, w=1, tlsCAFile=certifi.where())
 db = mongo_client['NLP-Cross-Cutting-Exposure']
@@ -22,50 +24,54 @@ visited_users = set()
 
 # Get article data for whole page
 def get_article_data(page):
-    json_selector = 'script[type="application/ld+json"]'
-    json_content = page.inner_text(json_selector)
-    data = json.loads(json_content)
+    try:
+        json_selector = 'script[type="application/ld+json"]'
+        json_content = page.inner_text(json_selector)
+        data = json.loads(json_content)
 
-    wafer_json_content = page.inner_text('.wafer-caas-data[type="application/json"]')
-    data_wafer = json.loads(wafer_json_content)
+        wafer_json_content = page.inner_text('.wafer-caas-data[type="application/json"]')
+        data_wafer = json.loads(wafer_json_content)
 
-    og_url = page.evaluate('(document.querySelector("meta[property=\'og:url\']") || {}).content')
-    news_keywords = page.evaluate('(document.querySelector("meta[name=\'news_keywords\']") || {}).content')
-    og_title = page.evaluate('(document.querySelector("meta[property=\'og:title\']") || {}).content')
-    og_description = page.evaluate('(document.querySelector("meta[property=\'og:description\']") || {}).content')
-    og_image = page.evaluate('(document.querySelector("meta[property=\'og:image\']") || {}).content')
-    body = page.inner_text('.caas-body')
-    min_read = page.inner_text('.caas-attr-mins-read')
-    date_published = data.get("datePublished")
-    date_modified = data.get("dateModified")
-    authors = data.get('author')
-    num_comments = data_wafer.get('commentsCount')
+        og_url = page.evaluate('(document.querySelector("meta[property=\'og:url\']") || {}).content')
+        news_keywords = page.evaluate('(document.querySelector("meta[name=\'news_keywords\']") || {}).content')
+        og_title = page.evaluate('(document.querySelector("meta[property=\'og:title\']") || {}).content')
+        og_description = page.evaluate('(document.querySelector("meta[property=\'og:description\']") || {}).content')
+        og_image = page.evaluate('(document.querySelector("meta[property=\'og:image\']") || {}).content')
+        body = page.inner_text('.caas-body')
+        min_read = page.inner_text('.caas-attr-mins-read')
+        date_published = data.get("datePublished")
+        date_modified = data.get("dateModified")
+        authors = data.get('author')
+        num_comments = data_wafer.get('commentsCount')
 
-    news_outlet_link = data.get('provider').get('url')
-    news_outlet_name = data.get('provider').get('name')
+        news_outlet_link = data.get('provider').get('url')
+        news_outlet_name = data.get('provider').get('name')
 
-    script_content = page.locator('//*[@id="atomic"]/body/script[4]')
-    script_content.wait_for(state="attached")
-    page.evaluate(script_content.text_content())
-    category_label = page.evaluate('() => window.YAHOO.context.meta.categoryLabel')
+        script_content = page.locator('//*[@id="atomic"]/body/script[4]')
+        script_content.wait_for(state="attached")
+        page.evaluate(script_content.text_content())
+        category_label = page.evaluate('() => window.YAHOO.context.meta.categoryLabel')
 
-    print(og_title)
-    return {
-        "url": og_url,
-        "keywords": news_keywords,
-        "title": og_title,
-        "description": og_description,
-        "image_url": og_image,
-        "body": body,
-        "min_read": min_read,
-        "date_published": date_published,
-        "date_modified": date_modified,
-        "authors": authors,
-        "num_comments": num_comments,
-        "outlet_link": news_outlet_link,
-        "outlet_name": news_outlet_name,
-        "category": category_label
-    }
+        print(og_title)
+        return {
+            "url": og_url,
+            "keywords": news_keywords,
+            "title": og_title,
+            "description": og_description,
+            "image_url": og_image,
+            "body": body,
+            "min_read": min_read,
+            "date_published": date_published,
+            "date_modified": date_modified,
+            "authors": authors,
+            "num_comments": num_comments,
+            "outlet_link": news_outlet_link,
+            "outlet_name": news_outlet_name,
+            "category": category_label
+        }
+    except Exception as e:
+        print(e)
+        return None
 
 
 # Get General User Info
@@ -100,26 +106,45 @@ def get_general_user_info(iframe_locator):
 # Scroll down to load more comments
 def generate_more_comments(iframe_locator, _page):
     print("Scrolling down to load more comments from user")
-    generated_enough = False
-    i = 0
-    last_number_comments = 0
-    while not generated_enough:
-        comment_section_locator = 'div[class*="src-components-FeedItem-styles__IndexWrapper"]'
-        comment_section_elements = iframe_locator.locator(comment_section_locator).element_handles()
+    try:
+        generated_enough = False
+        i = 0
+        last_number_comments = 0
+        while not generated_enough:
+            comment_section_locator = 'div[class*="src-components-FeedItem-styles__IndexWrapper"]'
+            comment_section_elements = iframe_locator.locator(comment_section_locator).element_handles()
 
-        if comment_section_elements.__len__() >= 300:
-            generated_enough = True
-        elif last_number_comments == comment_section_elements.__len__():
-            generated_enough = True
-        else:
-            i += 1
-            for comment_section in comment_section_elements:
-                comment_section.dispose()
-            _page.mouse.wheel(0, 50000)
-            last_number_comments = comment_section_elements.__len__()
-            time.sleep(1)
+            if comment_section_elements.__len__() >= 300:
+                generated_enough = True
+            elif last_number_comments == comment_section_elements.__len__():
+                generated_enough = True
+            else:
+                i += 1
+                for comment_section in comment_section_elements:
+                    comment_section.dispose()
+                _page.mouse.wheel(0, 50000)
+                last_number_comments = comment_section_elements.__len__()
+                time.sleep(1)
 
-    _page.mouse.wheel(50000 * i, 0)
+        _page.mouse.wheel(50000 * i, 0)
+    except Exception as e:
+        print(e)
+    print("Finished scroll for more comments on user page")
+
+
+# Load read more comments
+def load_read_more_comments(iframe_locator):
+    read_more_locator = 'a[class*="src-components-FeedItem-styles__ShowMoreButton"]'
+    try:
+        read_more_buttons = iframe_locator.locator(read_more_locator).element_handles()
+        while read_more_buttons:
+            for read_more in read_more_buttons:
+                read_more.wait_for_element_state("stable")
+                read_more.click()
+                read_more.dispose()
+            read_more_buttons = iframe_locator.locator(read_more_locator).element_handles()
+    except Error as e:
+        print("Finished loading all sections")
 
 
 # Get Comment Section data with source article and comments
@@ -138,22 +163,19 @@ def parse_comment_sections(iframe_locator, _page, browser):
         if "news.yahoo.com" not in source_article:
             comment_section.dispose()
             continue
-        source_article_page = browser.new_page(ignore_https_errors=True,
-                                               user_agent="Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_10_3; en-US) "
-                                                          "Gecko/20100101 Firefox/55.8",
-                                               bypass_csp=True,
-                                               java_script_enabled=True,
-                                               service_workers="block",
-                                               reduced_motion="reduce")
+        source_article_page = create_new_page(browser)
         try:
-            source_article_page.goto(source_article, timeout=5000, wait_until="domcontentloaded")
+            navigate_to_page(source_article_page, source_article)
             source_article_data = get_article_data(source_article_page)
+            if source_article_data is None:
+                raise Exception("Source article data is None")
             source_article_page.close()
+            print("Got source article data")
         except Exception as e:
             print(e)
-            print("Finished parsing comments section")
-            comment_section.dispose()
             source_article_page.close()
+            comment_section.dispose()
+            print("Finished parsing comments section")
             continue
 
         # Parse each comment and type
@@ -224,24 +246,11 @@ def parse_users(iframe_locator, _page, browser):
 
             if user['username'] not in visited_users:
 
-                try:
-                    generate_more_comments(iframe_locator, _page)
-                    print("Finished scrolling for more comments on profile")
-                except Exception as e:
-                    print(e)
+                # Generate more comments by scrolling
+                generate_more_comments(iframe_locator, _page)
 
                 # Load Read More Comments
-                read_more_locator = 'a[class*="src-components-FeedItem-styles__ShowMoreButton"]'
-                try:
-                    read_more_buttons = iframe_locator.locator(read_more_locator).element_handles()
-                    while read_more_buttons:
-                        for read_more in read_more_buttons:
-                            read_more.wait_for_element_state("stable")
-                            read_more.click()
-                            read_more.dispose()
-                        read_more_buttons = iframe_locator.locator(read_more_locator).element_handles()
-                except Error as e:
-                    print("Finished loading all sections")
+                load_read_more_comments(iframe_locator)
 
                 # Parse comments under a single source article
                 try:
@@ -263,16 +272,16 @@ def parse_users(iframe_locator, _page, browser):
                 print("finished scraping user")
                 continue
             else:
-                print("")
                 return users_objs
         except Exception as e:
             print(e)
+            break
 
     return users_objs
 
 
 # Find and open comments button
-def open_comments_button(_page, retries=5):
+def open_comments_button(_page):
     button_class = '.caas-button.view-cmts-cta.showCmtCount'
     button_element = _page.locator(button_class)
     button_element.scroll_into_view_if_needed()
@@ -282,7 +291,6 @@ def open_comments_button(_page, retries=5):
 
 # Get user data
 def get_users_data(_page, browser):
-    _page.set_default_timeout(5000)
     # Activate comment section
     try:
         open_comments_button(_page)
@@ -306,18 +314,6 @@ def get_users_data(_page, browser):
     return parse_users(iframe_locator, _page, browser)
 
 
-# Process each article by getting article and users data
-def process_article(_page, link, browser, retries=3):
-    for i in range(retries):
-        try:
-            _page.goto(link, timeout=5000, wait_until="domcontentloaded")
-            _article_data = get_article_data(_page)
-            user_data = get_users_data(_page, browser)
-            return _article_data, user_data
-        except Exception as e:
-            print(e)
-
-
 # Write array to MongoDB
 def write_to_mongodb(_collection, _array, id_field):
     try:
@@ -331,71 +327,111 @@ def write_to_mongodb(_collection, _array, id_field):
         print(e)
 
 
+# Navigate to page
+def navigate_to_page(page, link):
+    for i in range(0, PAGE_RETRIES):
+        try:
+            page.goto(link, timeout=5000, wait_until="domcontentloaded")
+            break
+        except Exception as e:
+            print(e)
+
+
+# Scrolls down to generate more articles
+def generate_more_articles(page, link):
+    duration = 30
+    for i in range(duration):
+        page.evaluate('window.scrollTo(0, document.body.scrollHeight);')
+        time.sleep(1)
+
+
+# Scrape Yahoo News section
+def scrape_section(link, p):
+    browser = create_new_browser(p)
+    page = create_new_page(browser)
+
+    navigate_to_page(page, link)
+    generate_more_articles(page, link)
+
+    section_article_data = []
+    section_users_data = []
+
+    stream_items = page.query_selector_all('.stream-item')
+    for stream_item in stream_items:
+        article_link = stream_item.query_selector('a').get_attribute('href')
+        if 'news.yahoo.com' not in article_link and "https://" not in article_link:
+            print(article_link)
+            article_link = 'https://news.yahoo.com' + article_link
+        if article_link not in visited_articles and article_link.__contains__(
+                '.html') and 'news.yahoo.com' in article_link:
+            visited_articles.add(article_link)
+            article_page = create_new_page(browser)
+            article_page.set_viewport_size({"width": 1600, "height": 1200})
+
+            navigate_to_page(article_page, article_link)
+
+            article_data = get_article_data(article_page)
+
+            if article_data is not None:
+                section_article_data.append(article_data)
+                users_data = get_users_data(article_page, browser)
+                if users_data is not None:
+                    section_users_data.extend(users_data)
+
+            article_page.close()
+
+    page.close()
+    browser.close()
+
+    return section_article_data, section_users_data
+
+
+# Create new page given browser
+def create_new_page(browser):
+    page = browser.new_page(ignore_https_errors=True,
+                            user_agent="Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_10_3; en-US) "
+                                       "Gecko/20100101 Firefox/55.8",
+                            bypass_csp=True,
+                            java_script_enabled=True,
+                            service_workers="block",
+                            reduced_motion="reduce")
+    page.set_default_timeout(5000)
+    return page
+
+
+# Create new browser
+def create_new_browser(p):
+    browser = p.chromium.launch(headless=True)
+    return browser
+
+
 # Run the job
 def job():
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=True)
+        browser = create_new_browser(p)
 
         articles = []
         users = []
 
-        # Parse through the articles on the scrolling page
-        # 'https://news.yahoo.com/tagged/donald-trump', 'https://news.yahoo.com/tagged/joe-biden'
-        for start_link in ['https://news.yahoo.com/politics/']:
-            try:
-                start_page = browser.new_page(ignore_https_errors=True,
-                                              user_agent="Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_10_3; en-US) "
-                                                         "Gecko/20100101 Firefox/55.8",
-                                              bypass_csp=True,
-                                              java_script_enabled=True,
-                                              service_workers="block",
-                                              reduced_motion="reduce")
+        landing_page = create_new_page(browser)
+        navigate_to_page(landing_page, "https://news.yahoo.com/")
+        nav_bar_elements = landing_page.query_selector_all('#ybar-navigation > div > ul > li')
+        nav_bar_elements = nav_bar_elements[0:3] + nav_bar_elements[4:8]
+        links = []
 
-                retries = 3
-                while retries > 0:
-                    try:
-                        start_page.goto(start_link, timeout=5000, wait_until="domcontentloaded")
-                        retries = 0
-                    except Exception as e:
-                        print(e)
-                        retries -= 1
+        for nav_bar_element in nav_bar_elements:
+            link = nav_bar_element.query_selector("a").get_attribute('href')
+            links.append(link)
 
-                # Scroll to the bottom
-                for i in range(10):
-                    start_page.evaluate('window.scrollTo(0, document.body.scrollHeight);')
-                    time.sleep(0.5)
-
-                # Parse through each article
-                stream_items = start_page.query_selector_all('.stream-item')
-                for stream_item in stream_items:
-                    item_link = stream_item.query_selector('a').get_attribute('href')
-
-                    if 'news.yahoo.com' not in item_link:
-                        item_link = 'https://news.yahoo.com' + item_link
-                    if item_link not in visited_articles and item_link.__contains__('.html'):
-                        visited_articles.add(item_link)
-                        current_page = browser.new_page(ignore_https_errors=True,
-                                                        user_agent="Mozilla/5.0 (Macintosh; U; Intel Mac OS X "
-                                                                   "10_10_3; en-US)"
-                                                                   "Gecko/20100101 Firefox/55.8",
-                                                        bypass_csp=True,
-                                                        java_script_enabled=True,
-                                                        service_workers="block",
-                                                        reduced_motion="reduce")
-                        current_page.set_viewport_size({"width": 1600, "height": 1200})
-                        article_data, users_data = process_article(current_page, item_link, browser)
-
-                        if article_data is not None:
-                            articles.append(article_data)
-                        if users_data is not None:
-                            users.extend(users_data)
-
-                        current_page.close()
-            except Exception as e:
-                print(e)
-
-        # Close resources
+        landing_page.close()
         browser.close()
+
+        for link in links:
+            section_articles, section_users = scrape_section(link, p)
+            if section_articles is not None:
+                articles.extend(section_articles)
+                if section_users is not None:
+                    users.extend(section_users)
 
         # Write to MongoDB
         collection_articles = db['Articles']
@@ -413,9 +449,11 @@ def job():
         visited_users.clear()
 
 
-schedule.every().day.at("00:00").do(job)
-schedule.every().day.at("12:00").do(job)
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+# schedule.every().day.at("00:00").do(job)
+# schedule.every().day.at("12:00").do(job)
+# while True:
+#     schedule.run_pending()
+#     time.sleep(1)
 
+if __name__ == '__main__':
+    job()
